8673165594037431c961aea08b743d4b99c44d76
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-09 22:13:17 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+        System.out.println("Enter a noun: ");
+        String noun = input.nextLine();
+        System.out.println("Enter a verb: ");
+        String verb = input.nextLine();
+        System.out.println("Enter an adjective: ");
+        String adjective = input.nextLine();
+        System.out.println("Enter an adverb: ");
+        String adverb = input.nextLine();
+
+        System.out.println("Do you "+verb+" your "+adjective+" "+noun+" "+adverb+"?");
+    }
+}
